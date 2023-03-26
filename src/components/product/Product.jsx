ad5d2dd3f0c410e4463956b226170ee449759a03
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-03-27 01:44:55 +0600

    complete the cart design

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {img, name, seller, quantity, price} =  props.product;
+    const {img, name, seller, ratings, quantity, price} =  props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+            <h3 className='product-name'>{name}</h3>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} starts</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
